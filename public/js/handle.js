035b9b040209268dddd82086f4dafbd9c69942a8
Nin7aStar <xiaoguangyu361@gmail.com>
2017-03-14 17:00:28 +0800

    top move action

--- public/js/handle.js
+++ public/js/handle.js
@@ -1,1 +1,27 @@
+var x = 0;
 
+setInterval(function () {
+    x += 5;
+    $('#highway').animate({'top': x + 'px'}, 50);
+}, 90);
+
+var moveTop = function() {
+    setInterval(function () {
+        x += 5;
+        $('#highway').animate({'top': x + 'px'}, 50);
+    }, 90);
+};
+
+var moveLeft = function () {
+    setInterval(function () {
+        x += 5;
+        $('#highway').animate({'left': x + 'px'}, 50);
+    }, 90);
+};
+
+var moveRight = function () {
+    setInterval(function () {
+        x += 5;
+        $('#highway').animate({'right': x + 'px'}, 50);
+    }, 90);
+};
