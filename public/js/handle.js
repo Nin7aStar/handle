57d506abee0ab58cd80a9201448fe0779c5201f0
Nin7aStar <xiaoguangyu361@gmail.com>
2017-03-14 21:56:22 +0800

    modify the playground style

--- public/js/handle.js
+++ public/js/handle.js
@@ -1,10 +1,18 @@
 var x = 0;
 
+// view area coordinate (x, y : width, height)
+var boundX = document.getElementById('view-area').offsetWidth;
+var boundY = document.getElementById('view-area').offsetHeight;
+
+// arrow keypress
+
+
 setInterval(function () {
     x += 5;
     $('#highway').animate({'top': x + 'px'}, 50);
 }, 90);
 
+// move to top
 var moveTop = function() {
     setInterval(function () {
         x += 5;
@@ -12,6 +20,7 @@ var moveTop = function() {
     }, 90);
 };
 
+// move to left
 var moveLeft = function () {
     setInterval(function () {
         x += 5;
@@ -19,6 +28,7 @@ var moveLeft = function () {
     }, 90);
 };
 
+// move to right
 var moveRight = function () {
     setInterval(function () {
         x += 5;
