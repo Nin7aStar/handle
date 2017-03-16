3ef53ec0121cf3f8ce22812cf2d516b24b3bb845
Nin7aStar <xiaoguangyu361@gmail.com>
2017-03-17 00:13:52 +0800

    modify the playground style

--- public/js/handle.js
+++ public/js/handle.js
@@ -1,37 +1,52 @@
-var x = 0;
+var KEY_UP = 38,
+    KEY_LEFT = 37,
+    KEY_RIGHT = 39;
 
-// view area coordinate (x, y : width, height)
-var boundX = document.getElementById('view-area').offsetWidth;
-var boundY = document.getElementById('view-area').offsetHeight;
+var carImages = ['top', 'left', 'right'];
 
-// arrow keypress
+$(function () {
+    var x = 0;
+
+    // view area coordinate (x, y : width, height)
+    var boundX = document.getElementById('view-area').offsetWidth;
+    var boundY = document.getElementById('view-area').offsetHeight;
+
+    // arrow keypress
 
 
-setInterval(function () {
-    x += 5;
-    $('#highway').animate({'top': x + 'px'}, 50);
-}, 90);
-
-// move to top
-var moveTop = function() {
     setInterval(function () {
         x += 5;
         $('#highway').animate({'top': x + 'px'}, 50);
-    }, 90);
-};
 
-// move to left
-var moveLeft = function () {
-    setInterval(function () {
-        x += 5;
-        $('#highway').animate({'left': x + 'px'}, 50);
+        if (x > 800) {
+            console.log("out of area");
+            x = 0;
+        }
     }, 90);
-};
 
-// move to right
-var moveRight = function () {
-    setInterval(function () {
-        x += 5;
-        $('#highway').animate({'right': x + 'px'}, 50);
-    }, 90);
-};
+
+    // move to top
+    var moveTop = function () {
+        setInterval(function () {
+            x += 5;
+            $('#highway').animate({'top': x + 'px'}, 50);
+        }, 90);
+    };
+
+    // move to left
+    var moveLeft = function () {
+        setInterval(function () {
+            x += 5;
+            $('#highway').animate({'left': x + 'px'}, 50);
+        }, 90);
+    };
+
+    // move to right
+    var moveRight = function () {
+        setInterval(function () {
+            x += 5;
+            $('#highway').animate({'right': x + 'px'}, 50);
+        }, 90);
+    };
+});
+
