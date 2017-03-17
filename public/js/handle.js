b848a89aa27251efe05ec3889e5abf7d46b016ef
Nin7aStar <xiaoguangyu361@gmail.com>
2017-03-18 01:17:27 +0800

    modify the playground style

--- public/js/handle.js
+++ public/js/handle.js
@@ -11,25 +11,84 @@ $(function () {
     var boundX = document.getElementById('view-area').offsetWidth;
     var boundY = document.getElementById('view-area').offsetHeight;
 
-    // arrow keypress
+    /**
+     *
+     */
+    function resize() {
+        // @todo
+        var MIN_GAME_WIDTH = 800;
+        var MIN_GAME_HEIGHT = 800;
+        var GAME_ASPECT_RATIO = 16 / 9;
 
+        var width = window.innerWidth;
+        var height = window.innerHeight;
 
-    setInterval(function () {
-        x += 5;
-        $('#highway').animate({'top': x + 'px'}, 50);
+        var gWidth, gHeight;
 
-        if (x > 800) {
-            console.log("out of area");
-            x = 0;
+        if(width < MIN_GAME_WIDTH || height < MIN_GAME_HEIGHT) {
+            gWidth = MIN_GAME_WIDTH;
+            gHeight = MIN_GAME_HEIGHT;
         }
-    }, 90);
+        else if ((width / height) > GAME_ASPECT_RATIO) {
+            // width is too large for height
+            gHeight = height;
+            gWidth = height * GAME_ASPECT_RATIO;
+        }
+        else {
+            //  height is too large for width
+            gWidth = width;
+            gHeight = width / GAME_ASPECT_RATIO;
+        }
+        resizeGame(gWidth, gHeight, GAME_ASPECT_RATIO);
+    }
 
+    resize();
+
+    /**
+     *
+     * @param gWidth
+     * @param gHeight
+     * @param aspectRatio
+     */
+    function resizeGame(gWidth, gHeight, aspectRatio) {
+        gWidth += "px";
+        gHeight += "px";
+
+        var gSection = document.getElementById("way-block-a");
+        var lPanel = document.getElementById("leftPanel");
+        var cPanel = document.getElementById("centerPanel");
+        var rPanel = document.getElementById("rightPanel");
+
+        gSection.style.height = gHeight;
+        gSection.style.width = gWidth;
+
+        // should the below be taken care of in the CSS?
+        lPanel.style.height = gHeight;
+        cPanel.style.height = gHeight;
+        rPanel.style.height = gHeight;
+
+        cPanel.style.width = gWidth;
+        lPanel.style.width = gWidth;
+        rPanel.style.width = lPanel.style.width;
+    }
+
+    window.onresize = resize;
+
+    // setInterval(function () {
+    //     x += 5;
+    //     $('.way').animate({'top': x + 'px'}, 50);
+    //
+    //     if (x > 800) {
+    //         console.log("out of area");
+    //         x = 0;
+    //     }
+    // }, 90);
 
     // move to top
     var moveTop = function () {
         setInterval(function () {
             x += 5;
-            $('#highway').animate({'top': x + 'px'}, 50);
+            $('.way').animate({'top': x + 'px'}, 50);
         }, 90);
     };
 
@@ -37,7 +96,7 @@ $(function () {
     var moveLeft = function () {
         setInterval(function () {
             x += 5;
-            $('#highway').animate({'left': x + 'px'}, 50);
+            $('.way').animate({'left': x + 'px'}, 50);
         }, 90);
     };
 
@@ -45,7 +104,7 @@ $(function () {
     var moveRight = function () {
         setInterval(function () {
             x += 5;
-            $('#highway').animate({'right': x + 'px'}, 50);
+            $('.way').animate({'right': x + 'px'}, 50);
         }, 90);
     };
 });
